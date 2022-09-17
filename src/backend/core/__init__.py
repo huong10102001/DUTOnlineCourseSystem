2b2ca2a1d2a383c7232ee407a231e367bbe3c321
Bang Pham <bangpham2501@gmail.com>
2022-09-17 14:32:28 +0700

    Setup env

--- src/backend/core/__init__.py
+++
@@ -1,9 +0,0 @@
-try:
-    import ujson
-    import requests
-
-    # noinspection PyUnresolvedReferences
-    requests.compat.json = ujson
-    requests.models.complexjson = ujson
-except ImportError:
-    pass
