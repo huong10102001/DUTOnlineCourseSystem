3279945b3ebb9a24b1064edb69a226b0b2148962
BangPham <bang.pham@paradox.ai>
2022-09-27 21:38:25 +0700

    check webhook

--- src/backend/core/settings.py
+++ src/backend/core/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 from datetime import timedelta
-
+#import load dotenv
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
