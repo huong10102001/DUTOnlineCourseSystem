28c3b0a795be4770cc17696210ea28a2ad4cf331
BangPham <bang.pham@paradox.ai>
2022-11-30 15:34:10 +0700

    lear-96 coding api for reset password

---
+++ src/backend/api_auth/views/reset_password.py
@@ -0,0 +1,44 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.db.models import Q
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from drf_yasg import openapi
+from rest_framework.response import Response
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from api_user.models import Account, User
+from rest_framework import status
+from django.core.mail import send_mail
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
+
+
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([])
+def forgot_password(request):
+    data = request.data
+    email = data.get('email')
+    account = Account.objects.filter(email=email)
+    if account.exists():
+        account = account.first()
+        user = User.objects.get(account_id=account.id)
+        user_id = user.id
+        token = PasswordResetTokenGenerator().make_token(account)
+        current_site = request.META['HTTP_HOST']
+        absurl = 'http://'+current_site + '/api/v1/users/password-reset?user_id=' + str(user_id) + '&token=' + str(token)
+        email_body = f'Hello {user.full_name}, \n Use link below to reset your password  \n' + str(absurl)
+        send_mail(
+            'Reset your passsword',
+            email_body,
+            'elearningpbl6@gmail.com',
+            [account.email],
+            fail_silently=False,
+        )
+    else:
+        return Response({'Error': 'Your email not exists!'}, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response({'success': 'We have sent you a link to reset your password.'}, status=status.HTTP_200_OK)
--- src/backend/api_user/views/user.py
+++ src/backend/api_user/views/user.py
@@ -1,21 +1,25 @@
 from api_auth.serializers import RegisterSerializer
 from api_base.views import BaseViewSet
-from api_user.models import User
-from api_user.serializers import UserSerializer
+from api_user.models import User, Account
+from api_user.serializers import UserSerializer, SetNewPasswordSerializer
 from rest_framework import status
 from rest_framework.response import Response
-from api_auth.permissions import AdminPermission
+from api_auth.permissions import AdminPermission, UserPermission
 from rest_framework.permissions import IsAuthenticated
 
 from api_user.services import UserService
+from django.contrib.auth.hashers import check_password, make_password
+from rest_framework.decorators import action
+from django.utils.encoding import DjangoUnicodeDecodeError
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 
 class UserViewSet(BaseViewSet):
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [UserPermission]
     serializer_class = UserSerializer
     permission_map = {
-        "retrieve": [IsAuthenticated]
+        "retrieve": UserPermission
     }
 
     def create(self, request, *args, **kwargs):
@@ -37,3 +41,38 @@ class UserViewSet(BaseViewSet):
             return Response(res_data, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['post'], url_path="change-password", permission_classes=[UserPermission])
+    def change_password(self, request, *args, **kwargs):
+        user_obj = request.user.user
+        account = Account.objects.filter(id=user_obj.account_id)
+        if account.exists():
+            account = account.first()
+            if check_password(request.data["old_password"], account.password):
+                account.password = make_password(request.data["new_password"])
+                account.save()
+                return Response({'success': True, 'message': 'Changed password successfully!'}, status=status.HTTP_200_OK)
+        return Response({"details": "Incorrect username! Change password unsuccessfully."}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['put'], url_path="password-reset", permission_classes=[],
+            authentication_classes=[], serializer_class=SetNewPasswordSerializer)
+    def password_reset_complete(self, request, *args, **kwargs):
+        user_id = request.query_params.get("user_id")
+        token = request.query_params.get("token")
+        data = request.data.copy()
+        try:
+            user = User.objects.get(id=user_id)
+            account = Account.objects.get(id=user.account_id)
+            data['id'] = user.account.id
+            if not PasswordResetTokenGenerator().check_token(account, token):
+                return Response({'error': 'Token is not valid, please request a new one'},
+                                status=status.HTTP_400_BAD_REQUEST)
+            serializer = self.serializer_class(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.update(data, serializer)
+            return Response({'success': True, 'message': 'Password reset success'}, status=status.HTTP_200_OK)
+
+        except DjangoUnicodeDecodeError as identifier:
+            return Response({'error': 'Token is not valid, please request a new one'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
